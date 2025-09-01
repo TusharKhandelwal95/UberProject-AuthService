342b827182624f3c01eeed7975a6b490dd5a7fc7
TusharKhandelwal95 <khandelwaltushar95@gmail.com>
2025-09-01 15:29:09 +0530

    Auth added successfully

--- src/main/java/com/example/uberprojectauthservice/controllers/AuthController.java
+++ src/main/java/com/example/uberprojectauthservice/controllers/AuthController.java
@@ -1,23 +1,42 @@
 package com.example.uberprojectauthservice.controllers;
 
+import com.example.uberprojectauthservice.dto.AuthRequestDto;
+import com.example.uberprojectauthservice.dto.AuthResponseDto;
 import com.example.uberprojectauthservice.dto.PassengerDto;
 import com.example.uberprojectauthservice.dto.PassengerSignupRequestDto;
 import com.example.uberprojectauthservice.services.AuthService;
+import com.example.uberprojectauthservice.services.JwtService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
+    @Value("${cookie.expiry}")
+    private int cookieExpiry;
     private final AuthService authService;
 
-    public AuthController(AuthService authService) {
+    private final AuthenticationManager authenticationManager;
+
+    private final JwtService jwtService;
+
+    public AuthController(AuthService authService, AuthenticationManager authenticationManager, JwtService jwtService) {
         this.authService = authService;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
     @PostMapping("/signup/passenger")
@@ -25,4 +44,26 @@ public class AuthController {
         PassengerDto passengerDto = authService.signupPassenger(passengerSignupRequestDto);
         return new ResponseEntity<>(passengerDto, HttpStatus.CREATED);
     }
+
+    @PostMapping("/signin/passenger")
+    public ResponseEntity<?> signIn(@RequestBody AuthRequestDto authRequestDto, HttpServletResponse response) {
+        System.out.println("Request received " + authRequestDto.getEmail() + " " + authRequestDto.getPassword());
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDto.getEmail(), authRequestDto.getPassword()));
+        if(authentication.isAuthenticated()) {
+            System.out.println("User authenticated");
+            String jwtToken = jwtService.createToken(authRequestDto.getEmail());
+
+            ResponseCookie cookie = ResponseCookie.from("JwtToken", jwtToken)
+                    .httpOnly(false)
+                    .secure(false)
+                    .path("/")
+                    .maxAge(cookieExpiry)
+                    .build();
+            response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+            return new ResponseEntity<>(AuthResponseDto.builder().success(true).build(), HttpStatus.OK);
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
 }
